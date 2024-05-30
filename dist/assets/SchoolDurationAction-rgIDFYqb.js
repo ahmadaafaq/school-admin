9ad394d8ed2d8ba5ceaa083b46a8b1810f2751f0
talhaansari692 <talhaansari692@gmail.com>
2024-05-30 16:45:36 +0530

    Delete dist directory

--- dist/assets/SchoolDurationAction-rgIDFYqb.js
+++
@@ -1,1 +0,0 @@
-import{X as t}from"./index-zLCHO5gj.js";const r=o=>({type:t.SET_SCHOOL_DURATIONS,payload:o});export{r as s};
--- dist/assets/StudentAction-ARQ8Q4-i.js
+++
@@ -1,1 +0,0 @@
-import{X as s}from"./index-zLCHO5gj.js";const a=t=>({type:s.SET_STUDENTS,payload:t}),o=t=>({type:s.SET_ALL_STUDENTS,payload:t});export{a,o as s};
